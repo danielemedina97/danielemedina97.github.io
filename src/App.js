1b40c27ca86f292cb0a713cbfdbc9e7b32733925
Daniel Eduardo Medina <danielmedinacs97@gmail.com>
2021-11-10 18:04:42 -0800

    Added table to page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import {useEffect} from 'react';
-import Navbar from './resources/components/Navbar/Navbar';
-import selfImage from './resources/images/me.jpg'
+import Navbar from './resources/components/navbar/Navbar';
+import selfImage from './resources/images/me.jpg';
+import ReactCardFlip from 'react-card-flip';
+import { useState } from 'react';
+import CardFlip from './resources/components/card-flip/CardFlip';
 
 function App() {
   useEffect(() => {
     document.title = "Daniel's Portfolio";
     
   }, []);
+
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = () => {
+      setIsFlipped(!isFlipped);
+  }
 
   return (
     <div className="App">
@@ -21,12 +30,42 @@ function App() {
       <div id="portfolio-intro">
         <table id="portfolio-intro-info">
           <tr>
-            <th id="about-me-th">
-              About Me
+            <th id="me-th">
+               Me
             </th>
+            <th id="about-me-th">About Me</th>
           </tr>
           <tr>
-            <td><img id="about-me-pic" src={selfImage} alt="Graduation picture of myself"/></td>
+            <td id="about-me-pic-td"><img id="about-me-pic" src={selfImage} alt="Graduation picture of myself"/></td>
+            {/*<td><ReactCardFlip isFlipped{isFlipped} flipDirection="vertical"></ReactCardFlip></td>*/}
+
+            <td><ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+              <p onClick={handleClick} 
+              style={{backgroundColor: 'white',
+                      height:400,
+                      width:800,
+                      color: "white",
+                      display:'flex',
+                      justifyContent:"center",
+                      alignItems: "center",
+                      fontSize:50,
+                      fontWeight:"bold",
+                      backgroundImage: "linear-gradient(to right, rgb(197, 128, 0), rgb(230, 130, 0))"
+            }}>
+                <button id="about-me-button"></button></p>
+              <p onClick={handleClick} 
+              style={{backgroundColor: 'white',
+              height:400,
+              width:800,
+              color: "green",
+              display:"flex",
+              justifyContent:"center",
+              alignItems:"center",
+              fontSize: 50,
+              fontWeight: "bold"
+             }}>
+                 This is the back of the card</p>
+              </ReactCardFlip></td>
            </tr>
         </table>
       </div>
