4e90d3fa0bd05cd81e7c4c83b7d07093523875c6
Daniel Eduardo Medina <danielmedinacs97@gmail.com>
2021-11-25 18:09:37 -0800

    Changed design of site, simplified UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import {useEffect} from 'react';
 import Navbar from './resources/components/navbar/Navbar'
-
+import MeSmall from './resources/images/me-small.png'
+import Resume from './resources/files/Resume.pdf';
+import GitHubIcon from './resources/images/github-icon.png';
+import LinkedInIcon from './resources/images/linkedin-icon.png';
 function App() {
   useEffect(() => {
     document.title = "Daniel's Portfolio";
@@ -11,14 +14,23 @@ function App() {
     <div className="App">
      {/*Navbar*/}
      <Navbar/>
-    
      {/*Portfolio Introduction*/}
-     <div id="portfolio-intro">
-       <p>
-         <span id="intro-fn"> Daniel Medina </span> <br/>
-         <span id="intro-degree">Computer Science B.S. </span>
-       </p>
+     <div className="portfolio-intro">
+       <div>
+         <div id="portfolio-intro-content">
+         <p id="hello">Hello </p> 
+         <p id="my-name"> I am Daniel Medina </p> 
+         <p id="aspiration">An Aspiring Software Developer </p>
+         </div>
      </div>
+     <div className="social-media">
+       <p>Social Media</p>
+          <ul className= "social-media-options">
+            <li> <a href="https://github.com/danielemedina97" target="_blank"><img src={GitHubIcon} id="github-icon"/> </a></li>
+            <li> <a href="https://www.linkedin.com/in/daniel-medina-178638183/"><img src={LinkedInIcon} id="linkedin-icon"/> </a></li>
+          </ul>
+     </div>
+    </div>
     </div>
   );
 }
--- src/resources/components/Navbar/Navbar.js
+++ src/resources/components/Navbar/Navbar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Navbar.css';
+import GitHubIcon from '../../images/github-icon.png';
+import LinkedInIcon from '../../images/linkedin-icon.png';
+import Resume from '../../files/Resume.pdf';
 
 export default function Navbar() {
     return (
@@ -7,8 +10,13 @@ export default function Navbar() {
             <nav id="navbar"> 
                 <ul id = "navbar-options"> 
                     <li><a href="">Home </a></li> 
-                    <li><a href="">Projects </a></li> 
-                    <li><a href="">Websites </a></li>
+                    <li><a href={Resume} target="_blank">Resume </a></li>
+                    {/*}
+                    <span class="social-media">
+                    <li> <a href="https://github.com/danielemedina97" target="_blank"><img src={GitHubIcon} id="github-icon"/> </a></li>
+                    <li> <a href="https://www.linkedin.com/in/daniel-medina-178638183/"><img src={LinkedInIcon} id="linkedin-icon"/> </a></li>
+                    </span>
+    */}
                 </ul>
             </nav>
         </>
